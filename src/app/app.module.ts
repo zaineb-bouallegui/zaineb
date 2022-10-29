19f9a09d7f592d1127e4c923c50497e3ef22966d
zaineb-bouallegui <bouallagui.zaineb@esprit.tn>
2022-10-29 19:12:21 +0100

    submit test

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListUserComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/list-user/list-user.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from "../model/User";
+
+@Component({
+  selector: 'app-list-user',
+  templateUrl: './list-user.component.html',
+  styleUrls: ['./list-user.component.css']
+})
+export class ListUserComponent implements OnInit {
+  accountCat: string;
+public list:User[]=[
+  {
+    idCustomer: 1,
+    firstName: "Mila",
+    lastName: " Kunis",
+    birthDate: "1999-06-30",
+    accountCategory: "Admin",
+    email: "mila@kunis.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar3.png",
+    profession: "Software Engineer"
+  },
+  {
+    idCustomer: 2,
+    firstName: "George",
+    lastName: "Clooney",
+    birthDate: "1999-06-30",
+    accountCategory: "Customer",
+    email: "marlon@brando.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar2.png",
+    profession: "Software Engineer"
+  },
+  {
+    idCustomer: 3,
+    firstName: "George",
+    lastName: "Clooney",
+    birthDate: "1999-06-30",
+    accountCategory: "Customer",
+    email: "marlon@brando.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar1.png",
+    profession: "Software Engineer"
+  },
+  {
+    idCustomer: 4,
+    firstName: "Ryan",
+    lastName: "Gossling",
+    birthDate:"1999-06-30",
+    accountCategory: "Golden",
+    email: "Ryan@nicholson.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar4.png",
+    profession: "Software Engineer"
+  },
+  {
+    idCustomer: 5,
+    firstName: "Robert",
+    lastName: "Downey",
+    birthDate: "1999-06-30",
+    accountCategory: "Blocked Account",
+    email: "robert@nicholson.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar5.png",
+    profession: "Software Engineer"
+  }
+];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  delete(user: User){
+    let i = this.list.indexOf(user);
+    this.list.splice(i,1)
+  }
+  transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    });
+  }
+
+}
